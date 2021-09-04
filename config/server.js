f557762459dc3777079d157cec0df0ea7a130aa3
Jim963 <qu4ej3w.6@gmail.com>
2021-09-04 15:50:01 +0800

    port口改1337

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 80),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '1f049af35e490a03082486848f044010'),
