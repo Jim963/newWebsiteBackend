31999022dbd44120c699dd9d3986d6c67ea3e911
Jim963 <qu4ej3w.6@gmail.com>
2021-09-05 16:51:09 +0800

    改81port看看

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '127.0.0.1'),
-  port: env.int('PORT', 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 81),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '1f049af35e490a03082486848f044010'),
